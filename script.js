0149f867fe3eae2193dd141f30568b71bdd56a75
fguzman717 <felixguzman717@gmail.com>
2025-05-24 11:02:49 -0500

    Completed the Button Functionality
    The button's click event now creates a new grid while checking to see if an old one exists and removes it if it does. The maximum grid size will be 100x100, so a
    conditional was also added to see if the number entered is acceptable otherwise it will default to the maximum of 100.

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-// TODO:
-// * Create the divs using JavaScript. Don’t try to create them by hand by copying and pasting them in your HTML file!
-// * It’s best to put your grid squares inside a “container” div. This div can be written in your HTML file.
-// * Use Flexbox to make the divs appear as a grid (versus just one on each line). Despite the name, do not be tempted to research or use CSS Grid, as it will be taught in a later lesson after the foundations path. This project is an opportunity specifically to practice Flexbox!
-
-// This will be the "container" div
-// I could just add the "container" div to the html doc but I prefer to get some practice with DOM manipulation by strictly using just Javascript to add all of my elements
 const body = document.querySelector("body");
-const container = document.createElement("div");
-container.setAttribute("id", "container");
-body.appendChild(container);
 
-// Here is where we will append the 16x16 grid of divs
-// I was originally going to append each grid one by one using a for loop, but I found out that doing that within a documentFragment will be much more efficient by appending all of the divs at once
-const fragment = document.createDocumentFragment();
+// A button will placed above the grid and will prompt the user for a grid size
+const newGridButton = document.createElement("button");
+newGridButton.setAttribute("id", "new-grid-button");
+newGridButton.textContent = "New Grid";
+body.appendChild(newGridButton);
 
-for (let i = 1; i <= 256; i++) {
-  const gridDiv = document.createElement("div");
-  gridDiv.classList.add("grid-div");
-  // gridDiv.textContent = `${i}`;
-  fragment.appendChild(gridDiv);
+newGridButton.addEventListener("click", (event) => {
+  let gridSizePrompt = prompt("Enter squares per side (max 100):");
+  if (gridSizePrompt > 100) {
+    console.log(
+      "You have entered a number greater than 100! \nDefaulting to 100 ...."
+    );
+    gridSizePrompt = 100;
+  }
+  let gridSize = gridSizePrompt ** 2;
 
-  gridDiv.addEventListener("mouseover", (event) => {
-    event.target.style.background = "grey";
-  });
-}
+  // This will check if there is an existing grid. If so, it will remove the old one and continue with the event
+  let oldGrid = document.getElementById("container");
+  if (oldGrid !== null) {
+    console.log(
+      "Old Grid Exists! \n\nRemoving Old Grid ....\n\nCreating New Grid ...."
+    );
+    oldGrid.remove();
+  } else {
+    console.log("Grid Does NOT exist! \n\nCreating New Grid ....");
+  }
 
-container.appendChild(fragment);
+  // Container for all of the divs
+  const container = document.createElement("div");
+  container.setAttribute("id", "container");
+  body.appendChild(container);
+
+  // This, along with the for loop, will efficiently append all divs at once rather than one at a time.
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 1; i <= gridSize; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("grid-div");
+    fragment.appendChild(gridDiv);
+
+    gridDiv.addEventListener("mouseover", (event) => {
+      event.target.style.background = "grey";
+    });
+  }
+  container.appendChild(fragment);
+});
